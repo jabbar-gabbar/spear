54e1321fbef5d20d12b493a54f2c6d3a3d60e532
tarang patel <372647+jabbar-gabbar@users.noreply.github.com>
2021-11-29 22:50:23 -0600

    add ApiConfig

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    
+    let config = AppConfig {
+        source: String::from("source"),
+        destination: String::from("destination"),
+    };
+    println!(
+        "source {}, destination {}",
+        config.source, config.destination
+    );
+}
+
+struct AppConfig {
+    source: String,
+    destination: String,
 }
