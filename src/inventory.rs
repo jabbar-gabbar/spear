fe8affe80d3614ddf0e0dcdb6a268e061c2a95b4
tarang patel <372647+jabbar-gabbar@users.noreply.github.com>
2022-01-29 00:53:56 -0600

    add unit test

--- src/inventory.rs
+++ src/inventory.rs
@@ -1,5 +1,5 @@
-use std::fs::File;
 use std::fs;
+use std::fs::File;
 use std::io::Write;
 use std::io::{Error, ErrorKind};
 use std::path::Path;
@@ -29,7 +29,6 @@ pub fn list(read_to_string_impl: &dyn ReadToString) -> Result<Vec<String>, Error
 
     let mut lines: Vec<String> = Vec::new();
 
-    // unit test
     for line in content.file_content.lines() {
         lines.push(String::from(line));
     }
@@ -58,4 +57,25 @@ pub fn append(inventory_path: String) -> Result<(), Error> {
     let mut option = fs::OpenOptions::new();
     let mut file: File = option.append(true).create(true).open(inventory_path)?;
     Ok(file.write_all("text\n".as_bytes())?)
+}
+
+struct TestInventoryFile {
+    content: String,
+}
+impl ReadToString for TestInventoryFile {
+    fn read_to_string(&self) -> Result<ReadToStringResult, Error> {
+        Ok(ReadToStringResult {
+            file_content: String::from(&self.content),
+        })
+    }
+}
+
+#[cfg(inventory_tests)]
+#[test]
+fn list_returns_list() {
+    let expected: Vec<String> = vec!["test1".into(), "test2".into()];
+    let test_inventory_file = TestInventoryFile {
+        content: expected.join("\r\n"),
+    };
+    assert_eq!(list(&test_inventory_file).unwrap(), expected);
 }
---
+++ src/source.rs
@@ -0,0 +1,9 @@
+
+
+fn list_upload(){
+    inventory::list("inventory_path: String");
+
+    // get list of files from source path
+
+    // calculate list of files to be uploaded based  on the given lists
+}
